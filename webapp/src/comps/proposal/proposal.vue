ab88da5f4af6fae4294b9f579d24036283683b14
Luis Iván Cuende <me@luisivan.net>
2017-10-04 19:31:48 +0200

    Added date proposals end on (hardcoded)

--- webapp/src/comps/proposal/proposal.vue
+++ webapp/src/comps/proposal/proposal.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="proposal">
     <h3>{{ proposal.title }}</h3>
+    <div class="endDate">{{ proposal.endDate }}Ends on 15 Nov 2017</div>
     <ul>
       <li v-for="option in options" :key="option.id">
         <div>{{option.text}}</div>
@@ -39,19 +40,34 @@
     border: 1px solid #e8e8e8;
     padding: 10px 20px;
     margin-bottom: 20px;
+    transition: all 0.5s ease-in-out;
+    cursor: pointer;
+  }
+  .proposal:hover {
+    transform: translateY(-5px);
+    box-shadow: 0px 1px 5px rgba(var(--grey500-rgb), 0.1);
   }
   .proposal h3 {
     font-size: 18px;
     margin-top: 0;
     margin-bottom: 14px;
     font-weight: normal;
+    max-width: 66%;
+    display: inline-block;
+  }
+  .proposal .endDate {
+    font-size: 12px;
+    float: right;
+    text-align: center;
+    margin-top: 5px;
+    color: var(--grey600);
   }
   .proposal ul {
     list-style: none;
   }
   .proposal ul li div {
     display: inline-block;
-    width: 33%;
+    width: 40%;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
